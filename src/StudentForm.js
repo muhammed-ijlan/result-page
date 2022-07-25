6246995b2e2745825eab6ead20998b09ffd40640
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-25 13:41:57 +0530

    initial commit

---
+++ src/StudentForm.js
@@ -0,0 +1,113 @@
+import React, { useRef, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { students } from './dummy_data';
+import Table from 'react-bootstrap/Table';
+
+
+export default function StundentForm() {
+    const [validation, setValidation] = useState(false)
+    const regRef = useRef(null);
+    const dobRef = useRef(null);
+    const [currentStudent, setCurrentStudent] = useState("")
+
+    function registerinputHandler() {
+
+        console.log(regRef.current.value);
+    }
+    // function dobInputHandler() {
+
+    //     console.log(dobRef.current.value);
+    // }
+
+
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        students.forEach(student => {
+            if (student.registerNumber == regRef.current.value) {
+                if (student.dob == dobRef.current.value) {
+                    setValidation(true)
+                    setCurrentStudent(student)
+
+                } else {
+                    console.log("DOb is wrong");
+                }
+            } else {
+                console.log("Register Number is wrong");
+            }
+        }
+
+        )
+    }
+
+    return (
+        <>
+            {!validation && <Form onSubmit={submitHandler}>
+                <Form.Group className="mb-3" style={{ width: "20rem" }} controlId="formBasicEmail">
+                    <Form.Label>Register Number</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Register Number" ref={regRef} onChange={registerinputHandler} />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" style={{ width: "20rem" }} controlId="formBasicPassword">
+                    <Form.Label>DOB</Form.Label>
+                    <Form.Control type="text" placeholder="dd/mm/yy" ref={dobRef} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>}
+
+            {validation &&
+                <>
+                    <h1>Student Name: {currentStudent.name}</h1>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Subject</th>
+                                <th>Mark</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentStudent.marks.map(mark => (
+
+                                <>
+                                    <tr>
+                                        <td>Malayalam</td>
+                                        <td>{mark.malayalam}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        <td>{mark.english}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Maths</td>
+                                        <td>{mark.maths}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Hindi</td>
+                                        <td>{mark.hindi}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Total Mark</b></td>
+                                        <td>{parseInt(mark.malayalam + mark.hindi + mark.english + mark.maths)}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Total Percentage</b></td>
+                                        <td>{parseInt(mark.malayalam + mark.hindi + mark.english + mark.maths) * 100 / 400}%</td>
+                                    </tr>
+
+                                </>
+                            ))}
+
+                        </tbody>
+                    </Table>
+                    <Button variant="primary" onClick={() => setValidation(false)} > View Another Result  </Button>
+                </>}
+
+        </>
+    )
+}
---
+++ src/dummy_data.js
@@ -0,0 +1,42 @@
+export const students = [
+    {
+        name: "Rahul",
+        registerNumber: 100,
+        dob: "10/12/2000",
+        marks: [
+            {
+                malayalam: 48,
+                hindi: 85,
+                maths: 54,
+                english: 65
+            }
+        ]
+    },
+    {
+        name: "Sona",
+        registerNumber: 200,
+        dob: "10/11/2000",
+        marks: [
+            {
+                malayalam: 84,
+                hindi: 55,
+                maths: 52,
+                english: 65
+            }
+        ]
+    },
+    {
+        name: "Sam",
+        registerNumber: 300,
+        dob: "11/03/1999",
+        marks: [
+            {
+                malayalam: 18,
+                hindi: 12,
+                maths: 14,
+                english: 15
+            }
+        ]
+    },
+
+]
